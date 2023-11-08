eb1070c2f531990192b3157e5588164b5c77a5b4
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-11-08 21:36:26 +0300

    add solution javaSyntax task 0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 5;
+        int numericValue = 10;
+        int size = 15;
+        int number = 20;
+
+    }
+}
