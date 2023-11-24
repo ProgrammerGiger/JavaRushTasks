264cf72f930237f5fc3e68006a8de29a32fa72c1
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-11-24 23:40:42 +0300

    add solution javaSyntax task 0105

--- 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -7,8 +7,7 @@ package com.javarush.task.task01.task0105;
 public class Solution {
     public static void main(String[] args) {
         String name;
-        int age;
         String city;
-
+        int age;
     }
 }
