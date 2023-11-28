431d850a04da41275823fc0386eac19c64a750ea
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-11-28 21:16:01 +0300

    add solution javaSyntax task 0301

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
@@ -10,14 +10,12 @@ public class Solution {
     public static void main(String[] args) {
         String cold = "на улице холодно";
         String warm = "на улице тепло";
-
         Scanner scan = new Scanner(System.in);
         int temp = scan.nextInt();
-        if (temp >= 0) {
-            System.out.println(warm);
-        } else {
+        if (temp < 0) {
             System.out.println(cold);
+        } else if (temp >= 0) {
+            System.out.println(warm);
         }
-        
     }
 }
