8f4227fef094d9a0ba1fce9aaba21e70b1024852
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-11-28 21:21:10 +0300

    add solution javaSyntax task 0302

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         String militaryCommissar = ", явитесь в военкомат";
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        int age = sc.nextInt();
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        int age = scan.nextInt();
         if (age >= 18 && age <= 28) {
             System.out.println(name + militaryCommissar);
         }
