d89abfad0bbda6aec84afb81547550887721ca18
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-11-29 21:58:40 +0300

    add solution javaSyntax task 0411

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0411/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0411/Solution.java
@@ -6,10 +6,11 @@ package com.javarush.task.pro.task04.task0411;
 
 public class Solution {
     public static void main(String[] args) {
+        for (int i = 1; i < 15; i++) {
+            if ((i % 2) == 0) {
+                System.out.println(i);
+            }
 
-        for (int i = 1;i <= 15;i++) {
-            if (i % 2 == 0)
-            System.out.println(i);
         }
 
     }
