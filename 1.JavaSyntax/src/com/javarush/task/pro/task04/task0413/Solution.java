d72b80d96f06fc2c127b4913305e17f5108ac31d
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-11-30 22:14:18 +0300

    add solution javaSyntax task 0413

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
@@ -6,12 +6,18 @@ package com.javarush.task.pro.task04.task0413;
 
 public class Solution {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print('8');
-            }
 
+        for (int i = 0; i < 10; i++) {
+
+            int starCount = 1 + i;
+            for (int j = 0; j < starCount; j++) {
+
+                System.out.print("8");
+
+            }
             System.out.println();
+
         }
+
     }
 }
