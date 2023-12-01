890bf0025d7da9917347bb27f4299774fbb9cfe9
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-12-01 21:47:30 +0300

    add solution javaSyntax task 0304

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0304/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0304/Solution.java
@@ -8,16 +8,15 @@ import java.util.Scanner;
 
 public class Solution {
     private static boolean isHigh;
-    private static boolean isLow ;
+    private static boolean isLow;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double bodyTemperature = scanner.nextDouble();
-         isHigh = bodyTemperature > 37;
-         isLow = bodyTemperature < 36;
-        if (isHigh) {
+        Scanner scan = new Scanner(System.in);
+        double bodyTemperature = scan.nextDouble();
+
+        if (bodyTemperature > 37) {
             System.out.println("температура тела высокая");
-        } else if (isLow){
+        } else if (bodyTemperature < 36){
             System.out.println("температура тела низкая");
         } else {
             System.out.println("температура тела нормальная");
