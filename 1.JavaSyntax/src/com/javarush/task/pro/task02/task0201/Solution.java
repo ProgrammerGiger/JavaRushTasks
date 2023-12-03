1fb3c73cb1b6f86b93ddc263544b0214bb774340
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-12-03 15:47:40 +0300

    add solution javaSyntax task 0201

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -7,10 +7,10 @@ package com.javarush.task.pro.task02.task0201;
 public class Solution {
 
     public static void main(String[] args) {
-        int intValue = 1000;
-        int numericValue = 3000;
-        int size = 4500;
-        int number = 6400;
+        int intValue = 200;
+        int numericValue = 300;
+        int size = 600;
+        int number = 700;
 
     }
 }
