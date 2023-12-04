26867ae4fb70d99b9fb22181377c5b4ac2577ef9
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-12-04 21:10:21 +0300

    add solution javaSyntax task 0503

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
@@ -9,9 +9,9 @@ public class Solution {
     public static int[] array = new int[]{-1, 2, 3, -4, -5};
 
     public static void main(String[] args) {
-        for (int i = 0;i < array.length;i++) {
-            if (array[i] % 2 == 0) {
-                array[i] *= -1;
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] % 2) == 0) {
+                array[i] = array[i] * -1;
             }
 
         }
