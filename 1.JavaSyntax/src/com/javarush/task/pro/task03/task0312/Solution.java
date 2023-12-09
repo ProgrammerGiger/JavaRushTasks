333447270eec893a3aad61aaa67c903662c519ec
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-12-09 21:28:24 +0300

    add solution javaSyntax task 0312

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0312/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0312/Solution.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str1 = sc.nextLine();
-        String str2 = sc.nextLine();
+        Scanner console = new Scanner(System.in);
+        String str1 = console.nextLine();
+        String str2 = console.nextLine();
 
         if (str1.equals(str2))
             System.out.println("строки одинаковые");
