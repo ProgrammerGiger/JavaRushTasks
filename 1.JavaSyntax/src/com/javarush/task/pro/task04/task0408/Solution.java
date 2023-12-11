7ae61ef7f78dfe914bcdd7c700e52596baeb44b8
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-12-11 23:11:26 +0300

    add solution javaSyntax task 0408

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
@@ -9,10 +9,10 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
         int max = Integer.MIN_VALUE;
-        while (scan.hasNextInt()) {
-            int num = scan.nextInt();
+        while (console.hasNextInt()) {
+            int num = console.nextInt();
             if (num > max && (num % 2) == 0) {
                 max = num;
             }
