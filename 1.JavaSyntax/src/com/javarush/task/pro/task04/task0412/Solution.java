7dab9410d14fec84a3c71ef9b2655e43e2c342be
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-12-11 23:12:27 +0300

    add solution javaSyntax task 0412

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0412/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0412/Solution.java
@@ -8,10 +8,10 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int start = scanner.nextInt();
-        int end = scanner.nextInt();
-        int multiple = scanner.nextInt();
+        Scanner console = new Scanner(System.in);
+        int start = console.nextInt();
+        int end = console.nextInt();
+        int multiple = console.nextInt();
 
         int sum = 0;
         for (int i = start; i <= end ; i++) {
