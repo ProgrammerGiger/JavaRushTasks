0e34f31e1ec7c3ce520679e00df09103da7ca917
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-12-20 16:26:04 +0300

    add solution javaSyntax task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+    public static void universalMethod(int num) {
+
+    }
+    public static void universalMethod(String str) {
+
+    }
+    public static void universalMethod(double d) {
+
+    }
+    public static void universalMethod(boolean bool) {
+
+    }
+    public static void universalMethod(char ch) {
+
+    }
+    public static void universalMethod(float flo) {
+
+    }
+    public static void universalMethod(byte by) {
+
+    }
+    public static void universalMethod(long lo) {
+
+    }
+    public static void universalMethod(short sh) {
+
+    }
+}
