5d67e5c8376270855c15c04fd36ddcbe00b93fe3
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-12-25 23:07:26 +0300

    add solution javaSyntax task 0416

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -8,10 +8,10 @@ Share a Coke
 
 public class Solution {
     public static void main(String[] args) {
-       Scanner scan = new Scanner(System.in);
+       Scanner console = new Scanner(System.in);
 
-       int number1 = scan.nextInt();
-       int number2 = scan.nextInt();
+       int number1 = console.nextInt();
+       int number2 = console.nextInt();
        double separation = (number1 * 1.0 / number2);
         System.out.println(separation);
 
