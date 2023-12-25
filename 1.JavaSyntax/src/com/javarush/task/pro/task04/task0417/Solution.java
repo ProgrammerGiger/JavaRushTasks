2dc6477a1b4b2d727d4a85bf9f22a7fe59811792
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-12-26 01:11:00 +0300

    add solution javaSyntax task 0417

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0417/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0417/Solution.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int windSpeed = scan.nextInt();
+        Scanner console = new Scanner(System.in);
+        int windSpeed = console.nextInt();
         double converter = windSpeed * 3.6;
         int rounding = (int)Math.round(converter);
         System.out.println(rounding);
