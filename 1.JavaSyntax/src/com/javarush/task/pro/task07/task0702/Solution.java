ed866519967b2bd3a0b89efe211ea4ec2f7090b8
Dmitriy Sutormin <Sutormin2015@list.ru>
2023-12-28 01:10:46 +0300

    add solution javaSyntax task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte batya = 127;
+    short shorts = 32767;
+    int intel = 2147483647;
+    long bong = 9223372036854775807L;
+}
