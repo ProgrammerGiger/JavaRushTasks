44e858911c4f1e86297a82a616751206dac6c4de
Dmitriy Sutormin <Sutormin2015@list.ru>
2024-01-02 15:02:36 +0300

    add solution javaSyntax task 0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str1 = new String("Димооон");
+        String str2 = new String("Денчик");
+        String str3 = new String("Володя");
+        String str4 = new String("Юрец");
+        String str5 = new String("Антоха");
+        String str6 = new String("Комаров");
+        String str7 = new String("Клевер");
+        String str8 = new String("Кроули");
+        String str9 = new String("Кремень");
+        String str10 = new String("Дожжи");
+
+        int[] array1 = new int[1];
+        int[] array2 = new int[2];
+        int[] array3 = new int[3];
+        int[] array4 = new int[4];
+        int[] array5 = new int[5];
+
+        Scanner console1 = new Scanner(System.in);
+        Scanner console2 = new Scanner(System.in);
+    }
+}
