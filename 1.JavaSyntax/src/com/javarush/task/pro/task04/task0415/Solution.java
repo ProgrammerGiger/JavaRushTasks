becaf4e8c24d15f8f9b360ca861ca16bf92ca819
Dmitriy Sutormin <Sutormin2015@list.ru>
2024-01-05 22:42:47 +0300

    add solution javaSyntax task 0415

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        int radius = console.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int radius = scanner.nextInt();
         double pi = 3.14;
         int s = (int)(pi * radius * radius);
         System.out.println(s);
