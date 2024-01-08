849de71d9d51bea71243b162fd08ca2d4e8f637f
Dmitriy Sutormin <Sutormin2015@list.ru>
2024-01-08 21:04:50 +0300

    add solution javaSyntax task 0215

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -9,10 +9,10 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        int number1 = console.nextInt();
-        int number2 = console.nextInt();
-        int number3 = console.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int number1 = scan.nextInt();
+        int number2 = scan.nextInt();
+        int number3 = scan.nextInt();
 
         System.out.println((number1 + number2 + number3) / 3);
     }
