bf5f051a483484cc0e026b859978f0aecf020337
Dmitriy Sutormin <Sutormin2015@list.ru>
2024-01-18 22:23:07 +0300

    add solution javaSyntax task 1017

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        Eurasia eurasia = new Eurasia(54);
+        Africa africa = new Africa(30);
+        NorthAmerica northAmerica = new NorthAmerica(24);
+        SouthAmerica southAmerica = new SouthAmerica(17);
+        Antarctica antarctica = new Antarctica(14);
+        Australia australia = new Australia(7);
+    }
+}
+
+
+
+
+
