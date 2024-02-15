afc8e6d22338469ada7dd8d81f67274c4f2db280
DumskI <Sutormin2015@list.ru>
2024-02-15 23:02:20 +0300

    add solution javaSyntax task 1506

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1506/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task15.task1506;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Фейсконтроль
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
+
